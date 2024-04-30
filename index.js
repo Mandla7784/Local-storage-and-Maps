e9764d67d03f4e6228ec1f0854a7c8d746365b7c
Mandla Dyonase <Marsatti7784@gmail.com>
2024-04-30 05:23:00 +0000

    Added a map to remove duplicates on task list

--- index.js
+++ index.js
@@ -4,4 +4,20 @@ const taskList = [
   { id: 3, task: "join php class" },
 ];
 
-function readDataFromDatabase() {}
+function readDataFromDatabase() {
+  const map = new Map();
+  //looping on list to check if task alerady exits , or not ,set it to map
+  for (const task in taskList) {
+    map.set(task.id, task);
+  }
+
+  const uniqueTasks = Array.from(map.values());
+  localStorage.setItem("tasks", uniqueTasks);
+}
+
+function addingNewTask() {
+  const userTask = document.querySelector("");
+  const newTask = {
+    id: new Date().getTime().toString(),
+  };
+}
