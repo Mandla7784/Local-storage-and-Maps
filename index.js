65b41127085dc04b3e32d7a52446e3e9269b317b
Mandla Dyonase <Marsatti7784@gmail.com>
2024-04-30 06:06:51 +0000

    Added a task add function

--- index.js
+++ index.js
@@ -3,21 +3,55 @@ const taskList = [
   { id: 2, task: "Fix bugs on project" },
   { id: 3, task: "join php class" },
 ];
-
+readDataFromDatabase();
 function readDataFromDatabase() {
-  const map = new Map();
+  addingNewTask();
+  const uniqueTasks = new Map();
   //looping on list to check if task alerady exits , or not ,set it to map
-  for (const task in taskList) {
-    map.set(task.id, task);
-  }
+  taskList.forEach((task) => {
+    uniqueTasks.set(task.id, task);
+  });
+  const myTasks = Array.from(uniqueTasks.values());
+  localStorage.setItem("tasks", JSON.stringify(myTasks));
 
-  const uniqueTasks = Array.from(map.values());
-  localStorage.setItem("tasks", uniqueTasks);
+  const displayedTasks = JSON.parse(localStorage.getItem("tasks"));
+  console.log(displayedTasks);
+  let html = "";
+  displayedTasks.forEach((usertask) => {
+    html += `
+      <tr class="row-1">
+          <td>
+             <span>${usertask.task}</span>
+            <button class="btn-edit">Edit</button>
+            <button class="btn-delete">Delete</button>
+          </td>
+        </tr>
+     
+     
+     `;
+  });
+  document.querySelector(".added").innerHTML = html;
 }
+//adding new task to the task list ...
 
 function addingNewTask() {
-  const userTask = document.querySelector("");
+  const userTask = document.querySelector("#task").value;
   const newTask = {
     id: new Date().getTime().toString(),
+    task: userTask,
   };
+
+  taskList.push(newTask);
 }
+function formHandler() {
+  const form = document.getElementById("form");
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+  });
+}
+
+const addtaskButton = document.querySelector(".btn-add");
+addtaskButton.addEventListener("click", () => {
+  formHandler();
+  readDataFromDatabase();
+});
