d38e9118fe4320a85e5579d025cc4ad2a85d4a31
Mandla Dyonase <Marsatti7784@gmail.com>
2024-04-30 05:17:17 +0000

    Added read data function

---
+++ index.js
@@ -0,0 +1,7 @@
+const taskList = [
+  { id: 1, task: "Joing meeting on thursday" },
+  { id: 2, task: "Fix bugs on project" },
+  { id: 3, task: "join php class" },
+];
+
+function readDataFromDatabase() {}
